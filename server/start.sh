d3217355f32715c00c0d50809a5bd9135217ff21
humanfriend22 <humanfriend22@gmail.com>
2022-10-26 19:46:44 -0700

    add plugin downloader

--- server/start.sh
+++ server/start.sh
@@ -1,10 +1,19 @@
 #/usr/bin/bash
-cd $MC_SERVER_PATH
 
+SERVER_DIRECTORY="${HOME}/server"
+PLUGIN_DIRECTORY="${SERVER_DIRECTORY}/plugins"
+
+# Delete server.pipe
+cd $SERVER_DIRECTORY
 rm server.pipe
-
 mkfifo server.pipe
 
-java -Xms7500M -Xmx7500M -jar server.jar --nogui < server.pipe > ../out 2>&1 &
+# Download & Enable Latest Plugin
+cd $PLUGIN_DIRECTORY
+sudo rm *.jar *.jar.*
+curl -s https://api.github.com/repos/cookie-jar-smp/tpa-home-plugin/releases/latest | grep "browser_download_url.*jar" | cut -d : -f 2,3 | tr -d \" | sudo wget -qi -
 
+# Start Server
+cd $SERVER_DIRECTORY
+sudo java -Xms7500M -Xmx7500M -jar server.jar --nogui < server.pipe > ../out 2>&1 &
 sleep infinity > server.pipe &
